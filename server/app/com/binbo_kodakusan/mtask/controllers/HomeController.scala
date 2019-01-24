40c36574b801c2aaf8a9bf0464a749028025b1a6
marony <marony@binbo-kodakusan.com>
2019-01-24 09:53:52 +0900

    ログイン済みならば直接アプリに飛ばす

--- server/app/com/binbo_kodakusan/mtask/controllers/HomeController.scala
+++ server/app/com/binbo_kodakusan/mtask/controllers/HomeController.scala
@@ -1,10 +1,12 @@
 package com.binbo_kodakusan.mtask.controllers
 
-import com.binbo_kodakusan.mtask.models.Tables
+import com.binbo_kodakusan.mtask.Constants
+import com.binbo_kodakusan.mtask.models.{SessionState, Tables}
 import javax.inject._
 import play.api.{Configuration, Logger}
 import play.api.mvc._
-import com.binbo_kodakusan.mtask.services.UserQuery
+import com.binbo_kodakusan.mtask.services.{ToodledoApi, ToodledoService, UserQuery, UserService}
+import com.binbo_kodakusan.mtask.util.SessionUtil
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
@@ -14,7 +16,10 @@ import scala.util.{Failure, Success}
 class HomeController @Inject()
   (config: Configuration, cc: ControllerComponents)
   (userDAO: UserQuery)
-  (implicit ec: ExecutionContext, webJarsUtil: org.webjars.play.WebJarsUtil)
+  (implicit ec: ExecutionContext,
+   usersService: UserService,
+   tdApi: ToodledoApi, tdService: ToodledoService,
+   webJarsUtil: org.webjars.play.WebJarsUtil)
     extends AbstractController(cc) {
 
   /**
@@ -23,7 +28,29 @@ class HomeController @Inject()
     * @return
     */
   def index = Action { implicit request =>
-    Ok(views.html.index("タイトルだよ"))
+    SessionUtil.getTdSessionState(request.session) match {
+      case Some(state) =>
+        // セッションが有効ならば最初からアプリに飛ばす
+        // アカウント情報の取得
+        tdService.getAccountInfo(request, Some(state)) match {
+          case Right((accountInfo, _: SessionState)) =>
+            Logger.info(accountInfo.toString)
+            usersService.upsertAccountInfo(accountInfo, Some(0), Some(state))
+            // うまくいったらReactのアプリを表示
+            val session = SessionUtil.setTdSession(
+              // stateをセッションから削除
+              SessionUtil.remove(request.session, Constants.SessionName.TD_STATE),
+              // セッションに色々設定
+              state)
+            Redirect(routes.HomeController.app)
+              .withSession(session)
+          case Left(e) =>
+            Logger.error(e.toString)
+            Ok(views.html.index("タイトルだよ"))
+        }
+      case None =>
+        Ok(views.html.index("タイトルだよ"))
+    }
   }
 
   /**
